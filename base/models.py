365a1c1ab79a2dc6c3a270e7c14d35ae5cb04356
flamemeister <aldiyar.saken123@gmail.com>
2023-10-18 01:19:46 +0200

    creation of creat-room ur

--- base/models.py
+++ base/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
-    # host
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField(null = True, blank=True)
     # participants = models.
