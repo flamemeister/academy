13db39903cfeb9035b7fc7e0cf911fc13c06bc0d
flamemeister <aldiyar.saken123@gmail.com>
2023-10-22 23:04:36 +0200

    login and logout

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.db.models import Q
 from .models import Room
 from .forms import RoomForm
@@ -11,6 +13,29 @@ from .models import Topic
 #     {'id': 3, 'name': 'Students'},
 # ]
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username = username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+    context = {}
+    return render(request,'base/login_register.html', context)
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
@@ -57,3 +82,5 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
+
+
