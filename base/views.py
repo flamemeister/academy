6cd72ff3f34ced60dd05cdb3f35f53f6e7fa1b2d
flamemeister <aldiyar.saken123@gmail.com>
2023-10-23 19:45:16 +0200

    messages CRUD

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room
 from .forms import RoomForm
-from .models import Topic
+from .models import Topic, Message
 
 # rooms = [
 #     {'id': 1, 'name':'Lets learn science!'},
@@ -76,7 +76,17 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room' : room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk = room.id)
+    context = {'room' : room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -116,5 +126,17 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user :
+        return HttpResponse('Restricted: Unmatched roles')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj':message})
 
 
